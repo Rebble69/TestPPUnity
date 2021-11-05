1a05a56737ed99720724023e2a3827d3632467d9
Rebble69 <69352549+Rebble69@users.noreply.github.com>
2021-11-05 09:55:50 +0700

    not working yet
    havent finishrd player movement script

--- sussy amongus/Assets/Scripts/CameraMovement.cs
+++ sussy amongus/Assets/Scripts/CameraMovement.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("This Updates Camera Position Every Frame")]
+    [Space]
+    [SerializeField] private Transform CameraObject;
+
+    private Transform camPos;
+
+    private void Start()
     {
-        
+        camPos = transform.Find("CameraPos").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        CameraObject.SetPositionAndRotation(camPos.position, camPos.rotation);
         
     }
 }
--- sussy amongus/Assets/Scripts/PlayerMovemtn.cs
+++ sussy amongus/Assets/Scripts/PlayerMovemtn.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovemtn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Assignment Stuff")] 
+    public GameObject playerBody;
+
+    [Header("MovementValues")] 
+    public float moveSpeed = 12f;
+
+    //Private Var's
+    private Rigidbody rb;
+    private GameObject playerCapsule;
+    private Vector3 moveDirection;
+    private float movementMulti = 0.01f;
+    
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = playerBody.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputHandler();   
     }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void InputHandler()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * y + transform.right * x;
+    }
+
+    void MovePlayer()
+    {
+        rb.AddForce(moveDirection.normalized * moveSpeed * movementMulti, ForceMode.Impulse);
+    }
+    
 }
